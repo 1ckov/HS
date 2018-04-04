cca351726210f81b22b617776d95c16a5713f79c
Aleksandar <Aleksandar@192.168.0.241>
2018-04-04 18:42:36 +0200

    Reworked BACW

--- PR1/src/pr1/uebung02/BloodAlcoholContentWatson.java
+++ PR1/src/pr1/uebung02/BloodAlcoholContentWatson.java
@@ -6,15 +6,32 @@ import static pr.MakeItSimple.readString;
 
 public class BloodAlcoholContentWatson {
 
-	public static void main(String[] args) {
-
-		double alkInGramm;
-		double gewicht;
-		double alter;
-		double großeCm;
+	public static void bldAlcConWat(double alkInGramm, double gewicht, double grosseCm,String geschlecht){
 		double alkImBlut = 0;
 		double dichteBlut = 1.055;
 		double GKW = 0;
+		if (geschlecht.equals("m")) {
+			println("Bitte Alter eingeben: ");
+			double alter = readDouble();
+
+			GKW = 2.447 - (0.09516 * alter) + (0.1074 * grosseCm) + (0.3362 * gewicht);
+			alkImBlut = (0.8 * alkInGramm) / (GKW * dichteBlut);
+			println("Eine männliche Person hat den Eingaben zufolge eine Blutalkoholkonzentration von: " + alkImBlut
+					+ " ‰");
+
+		}
+		else if (geschlecht.equals("w")) {
+			GKW = 0.203 - (0.07) + (0.1069 * grosseCm) + (0.2466 * gewicht);
+			alkImBlut = (0.8 * alkInGramm) / (GKW * dichteBlut);
+			println("Eine weibliche Person hat den Eingaben zufolge eine Blutalkoholkonzentration von: " + alkImBlut
+					+ " ‰");
+
+		}
+	}
+	
+	public static void main(String[] args) {
+
+		double gewicht, alkInGramm, grosseCm;
 
 		println("Bitte Körpergewicht in Kilogramm (kg) eingeben: ");
 		gewicht = readDouble();
@@ -23,31 +40,13 @@ public class BloodAlcoholContentWatson {
 		alkInGramm = readDouble();
 
 		println("Bitte Körpergröße in Centimeter (cm) eingeben: ");
-		großeCm = readDouble();
+		grosseCm = readDouble();
 
 		String geschlecht = "";
-		while (!geschlecht.equals("m") || !geschlecht.equals("w")) {
-			println("Bitte Geschlecht (m/w) eingeben: ");
-			geschlecht = readString();
-		}
-
-		if (geschlecht.equals("m")) {
-			println("Bitte Alter eingeben: ");
-			alter = readDouble();
-
-			GKW = 2.447 - (0.09516 * alter) + (0.1074 * großeCm) + (0.3362 * gewicht);
-			alkImBlut = (0.8 * alkInGramm) / (GKW * dichteBlut);
-			println("Eine männliche Person hat den Eingaben zufolge eine Blutalkoholkonzentration von: " + alkImBlut
-					+ " ‰");
-
-		}
-		else if (geschlecht.equals("w")) {
-			GKW = 0.203 - (0.07) + (0.1069 * großeCm) + (0.2466 * gewicht);
-			alkImBlut = (0.8 * alkInGramm) / (GKW * dichteBlut);
-			println("Eine weibliche Person hat den Eingaben zufolge eine Blutalkoholkonzentration von: " + alkImBlut
-					+ " ‰");
-
-		}
+		println("Bitte Geschlecht (m/w) eingeben: ");
+		geschlecht = readString();
+		
+		bldAlcConWat(alkInGramm, gewicht, grosseCm, geschlecht);
 	}
-
 }
+
